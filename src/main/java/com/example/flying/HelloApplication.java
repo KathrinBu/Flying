efea70c65fed9d8339e3bd45c558b63cef796e5e
Kate Bugrova <bugrovaev@mail.ru>
2023-09-07 23:30:46 +0300

    --

--- src/main/java/com/example/flying/HelloApplication.java
+++ src/main/java/com/example/flying/HelloApplication.java
@@ -2,8 +2,6 @@ package com.example.flying;
 import java.sql.*;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,13 +19,16 @@ public class HelloApplication extends Application {
     public static void main(String[] args) throws SQLException {
         Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/world","root","Pa$$w0rd");
         System.out.println("it's ok!");
-        String sql="SELECT * FROM city";
+    //    String sql="SELECT * FROM city";
         Statement statement= connection.createStatement();
-        ResultSet resultSet=statement.executeQuery(sql);
-        while (resultSet.next()){
-            String id= resultSet.getString("District");
-            System.out.println("---"+id);
-        }
+//        ResultSet resultSet=statement.executeQuery(sql);
+//        while (resultSet.next()){
+//            String idy = resultSet.getString("District");
+//            System.out.println("---"+ idy);
+//        }
+        statement.executeUpdate("CREATE TABLE IF NOT EXIST Books(id MEDIUMINT NOT NULL AUTO_INCREMENT, NAME CHAR(30) NOT NULL, PRIMARY_KEY(id) )");
+        statement.executeUpdate("insert into Books (name) values('Inferno')");
+        statement.executeUpdate("insert into Books set name='Solo'");
 
        // launch();
     }
